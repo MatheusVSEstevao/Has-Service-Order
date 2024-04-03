6e74a4c16635d413472aeb5029b55f7089329a39
Matheus <matheusvictor1292@gmail.com>
2024-04-02 22:16:10 -0300

    a

---
+++ OsDsII.api/Repository/ServiceOrderRepository.cs
@@ -0,0 +1,18 @@
+﻿using OsDsII.api.Data;
+
+namespace OsDsII.api.Repository
+{
+    public sealed class ServiceOrderRepository
+    {
+        private readonly DataContext _dataContext;
+        public ServiceOrderRepository(DataContext dataContext) 
+        {
+            _dataContext = dataContext;
+        }
+
+        public async Task<List<ServiceOrder>>GetAllAsync()
+        {
+            return await
+        }
+    }
+}
